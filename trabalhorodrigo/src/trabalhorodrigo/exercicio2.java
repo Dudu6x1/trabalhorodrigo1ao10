272e2f7055e0df7c926450520db07896ad291bfe
Dudu6x1 <dudu14716@gmail.comgit config --global user.email dudu14716@gmail.comgit config --global user.email dudu14716@gmail.com>
2022-10-31 00:14:04 -0300

    exercicios do trabalho

---
+++ trabalhorodrigo/src/trabalhorodrigo/exercicio2.java
@@ -0,0 +1,19 @@
+package trabalhorodrigo;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		
+		int a[]= {1,2,3,4,5,6,7,8,9,10};
+		int b[]= {1,2,3,4,5,6,7,8,9,10};
+		int c[]=new int[10];
+		int resultado;
+		
+		for(int i=0;i < a.length; ++i) {
+			resultado=a[i]+b[i];
+			c[i]=resultado;
+			System.out.println("O vetor A tem["+a[i]+"] + ["+b[i]+"]que tem o vetor B,que resulta no vetor C=["+c[i]+"]");			
+		}
+	}
+}
+
